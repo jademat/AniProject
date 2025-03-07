929e3bebd28431490ffba164f41a2a722a99a309
이혜미 <svsvdvdv@naver.com>
2025-03-07 18:02:17 +0900

    AdoptionController

---
+++ src/main/java/project/animalfoot/aniproject/controller/user/AdoptionController.java
@@ -0,0 +1,28 @@
+package project.animalfoot.aniproject.controller.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/adoption")
+@RequiredArgsConstructor
+@Slf4j
+public class AdoptionController {
+
+    @GetMapping("/write")
+    public String write() {
+        return "views/user/adoption/write";
+    }
+
+    @GetMapping("/list")
+    public String list() {
+        return "views/user/adoption/list";
+    }
+    @GetMapping("/view")
+    public String view() {
+        return "views/user/adoption/view";
+    }
+}
